a6017a29cd0397acf87c9722d759414c30514cfb
Nonzulu <knzulu16@gmail.com>
2018-05-31 10:36:19 +0200

    adding enduro portfolio

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'nzulu',
+			tags: [],
+			salt: 'c4e0f1c5336e14f4bfd0eaff4cf5db0d',
+			hash: '56ca63c24e8c05aa27be625b67f2c2dfa532995bca0e364e987c620b17ff1df3',
+			user_created_timestamp: 1527578866689
+		}
+	]
+})
---
+++ cms/index.js
@@ -0,0 +1,100 @@
+({
+	name: 'Nonzulu Kala',
+	specialty: '',
+	$main_image_type: 'image',
+	main_image: '/remote/direct_uploads/1527671662_ScreenShot2018-05-30at11.13.36.png',
+	$background_type: 'textarea',
+	background: 'I am Nonzulu Kala ,I am a professional software developer who is still growing in learning software development, I have completed a one year programme at Project CodeX, in agile fullStack web development. My passion is all about providing solutions to problems ,creating real elegant looking web applications. Keeping it simple and clean with added functionality of user interaction. We learn\'t JavaScript , Node.JS, Handlebar.JS, Express.JS, MongoDB, Test Driven Development(TDD) using mocha, HTML, CSS, Git and Agile principles of development such as Sprint planning, daily Standups, demonstration and Retrospective. Every end of the term we did 4 days Hackathon where we built solutions for problems presented by our sponsor such as Jumo and Steltix.',
+	$projects_min: 1,
+	projects: [
+		{
+			title: 'Basic web page',
+			$image_type: 'image',
+			image: '/remote/direct_uploads/1527671831_ScreenShot2018-05-30at11.05.15.png',
+			link: 'https://knzulu16.github.io/'
+		},
+		{
+			title: 'Greeting web page',
+			$image_type: 'image',
+			image: '/remote/direct_uploads/1527672343_ScreenShot2018-05-30at11.24.43.png',
+			link: 'https://knzulu16.github.io/git-Greeting'
+		},
+		{
+			title: 'Registration web page',
+			$image_type: 'image',
+			image: '/remote/direct_uploads/1527672528_ScreenShot2018-05-30at11.28.07.png',
+			link: 'https://knzulu16.github.io/Registration_Number'
+		},
+		{
+			title: 'Matching days webpage',
+			$image_type: 'image',
+			image: '/remote/direct_uploads/1527675108_ScreenShot2018-05-30at12.09.40.png',
+			link: 'https://knzulu16.github.io/MachingDays'
+		},
+		{
+			title: 'Shoe Catalogue web page',
+			$image_type: 'image',
+			image: '/remote/direct_uploads/1527675522_ScreenShot2018-05-30at12.17.44.png',
+			link: 'https://knzulu16.github.io/ShoeCatalogue'
+		},
+		{
+			title: 'Everyday decision',
+			$image_type: 'image',
+			image: '/remote/direct_uploads/1527675680_ScreenShot2018-05-30at12.20.52.png',
+			link: 'https://knzulu16.github.io/myDecision'
+		},
+		{
+			title: '',
+			$image_type: 'image',
+			image: '',
+			link: ''
+		}
+	],
+	$social_max: 3,
+	$social_min: 1,
+	social: [
+		{
+			$icon_type: 'fa',
+			icon: 'github',
+			text: 'Check out my code on github',
+			link: 'https://github.com/knzulu16'
+		},
+		{
+			$icon_type: 'fa',
+			icon: 'linkedin',
+			text: 'View my profile at LinkedIn',
+			link: 'https://www.linkedin.com/'
+		},
+		{
+			$icon_type: 'fa',
+			icon: 'stack-overflow',
+			text: 'I am also active on stackoverflow',
+			link: 'http://stackoverflow.com/cv/martingottweis'
+		}
+	],
+	$hobby_background_type: 'image',
+	hobby_background: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880478_pexels-photo-92323.jpeg',
+	hobby_title: 'Book enthusiast',
+	hobby_subtitle: 'Books are the old-school internet',
+	cta_tagline: 'Need a deep learning software developer?',
+	cta_text: 'I am actively looking for new challenges',
+	contact_email: 'knzulu16@gmail.com',
+	footer_text: '2017 @enduro.js mirror theme',
+	$footer_background_color_type: 'colorpicker',
+	footer_background_color: '#7e0343',
+	page_title: 'Mirror - enduro.js theme',
+	display_edit_stamp: true,
+	$style_expander: true,
+	$style_info: 'won\'t work with the temp action',
+	style: {
+		$background_color_size: 'inline',
+		$background_color_type: 'colorpicker',
+		background_color: '#ffffff',
+		$text_color_size: 'inline',
+		$text_color_type: 'colorpicker',
+		text_color: '#2b2928',
+		$accent_color_size: 'inline',
+		$accent_color_type: 'colorpicker',
+		accent_color: '#fafdff'
+	}
+})
